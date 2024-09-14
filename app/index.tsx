448e94dd435532e72168a70065a79b0630fd962b
frozenchewingum <c1aviclim@gmail.com>
2024-09-14 17:46:57 +0800

    setup transaction history module

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, Button } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Index() {
+
+  const router = useRouter();
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text> Edit app/index.tsx to edit this screen. </Text>
+    </View>
+  );
+}
